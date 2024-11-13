40413c4cfd9c393159a741817bb28c961b382a53
datptitudu2 <dattkdz@gmail.com>
2024-11-13 08:48:27 +0700

    update

--- Giuaki/todolist/src/Home.jsx
+++ Giuaki/todolist/src/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Create from './Create';
+
 import { BsFillCheckCircleFill, BsCircleFill, BsFillTrashFill } from 'react-icons/bs';
 
 function Home() {
